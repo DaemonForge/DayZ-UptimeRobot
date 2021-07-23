2f7881b882ec72d6b43f7dfd9d9332279613256e
Daemon Forge <kevhoddinott@gmail.com>
2021-07-23 14:42:17 -0400

    Update for new Universal API Update

--- UpTimeRobot/Scripts/5_Mission/MissionServer.c
+++ UpTimeRobot/Scripts/5_Mission/MissionServer.c
@@ -5,16 +5,20 @@ modded class MissionServer extends MissionBase
 	{
 		super.OnInit();
 		Print("[UPTIMEROBOT] Init URL: " + GetUpTimeRobotConfig().Url);
-		GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(this.SendUpTimePing, GetUpTimeRobotConfig().GetInterval(), true);
+		GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(this.SendUpTimePing, GetUpTimeRobotConfig().GetInterval(), false);
 	}
 	
 	
 	void SendUpTimePing(){
-		ref array<Man> players = new array<Man>;
+		GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(this.SendUpTimePing, GetUpTimeRobotConfig().GetInterval(), false);
+		autoptr array<Man> players = new array<Man>;
 		GetGame().GetPlayers( players );
 		if (players && players.Count() >= GetUpTimeRobotConfig().MinPlayers){
-			UApi().Rest().Get(GetUpTimeRobotConfig().Url);
-		} else {
+			#ifndef UNIVERSALAPI_STABLE
+				UApi().Rest().Get(GetUpTimeRobotConfig().Url);
+			#else
+				UApi().Get(GetUpTimeRobotConfig().Url);	
+			#endif
 		}
 	}
 }
